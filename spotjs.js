f68e5b8e4e013d9cf818724808f59251e25356a9
Tejas Birje <tejasbirje30@gmail.com>
2023-12-12 12:23:11 +0530

    Initial commit

---
+++ spotjs.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Spotify");
+
+let songs = [
+    {songname: "Blinding Lights",filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "Blinding Lights",filepath: "songs/2.mp3", coverpath: "covers/2.jpg"},
+    {songname: "Blinding Lights",filepath: "songs/3.mp3", coverpath: "covers/3.jpg"}
+
+]
+
+let audioElement = new Audio("1.mp3");
